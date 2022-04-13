573578a0af8b35eb6fb1004dd0a1d1b425c78b45
Robert Neville <gcab012204@coderacademy.edu.au>
2022-04-13 16:32:26 +1000

    More work on argument parser

---
+++ src/parser/parser.rb
@@ -0,0 +1,19 @@
+require_relative "./argument_parser"
+
+class Parser
+  def initialize
+    @parser = ArgumentParser.new
+    @parser.add_option("help", "h", false, false, false, 0, 0)
+    @parser.add_option("config", "c", true, false, false, 0, 2)
+  end
+
+  def parse(argv)
+    @parser.exec(argv)
+    results = {
+      args: @parser.parsed_args, 
+      options: @parser.parsed_options, 
+      errors: @parser.errors
+    }
+    results
+  end
+end
