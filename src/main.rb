b185b913623df7f1e5ba7074fd979156c27a5313
Robert Neville <gcab012204@coderacademy.edu.au>
2022-04-13 13:40:37 +1000

    Start work on app startup logic

---
+++ src/main.rb
@@ -0,0 +1,14 @@
+require "colorize"
+require_relative "./config_manager"
+
+config_manager = ConfigManager.new
+config_errors = config_manager.init
+
+if config_errors
+  puts "Oops! There were some configuration errors:"
+  config_errors.each do |error_message|
+    puts error_message.red
+  end
+  puts "Please run the setup script to sort things out!"
+  exit
+end
