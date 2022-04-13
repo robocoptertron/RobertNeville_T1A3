befef7af2416481fce18aad9e8828cb8c445e865
Robert Neville <gcab012204@coderacademy.edu.au>
2022-04-13 13:41:23 +1000

    Refactor search_places_by_name

--- src/lib/geocode.rb
+++ src/lib/geocode.rb
@@ -6,16 +6,22 @@ module Geocode
 
   def self.search_places_by_name(name)
     q = "q=#{name}"
-    response = HTTP.get("#{API_URL}#{q}&#{FORMAT}")
-    data = response.parse
-    places_found = []
-    data.each do |place_found|
-      places_found.push({
-        "display_name" => place_found["display_name"],
-        "latitude" => place_found["lat"],
-        "longitude" => place_found["lon"]
-      })
+    response = nil
+    begin
+      response = HTTP.get("#{API_URL}#{q}&#{FORMAT}")
+    rescue HTTP::ConnectionError
+      {error: true}
+    else
+      data = response.parse
+      places_found = []
+      data.each do |place_found|
+        places_found.push({
+          "display_name" => place_found["display_name"],
+          "latitude" => place_found["lat"],
+          "longitude" => place_found["lon"]
+        })
+      end
+      {error: false, places_found: places_found}
     end
-    places_found
   end
 end
