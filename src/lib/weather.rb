90bad5d9467bd7e47e0197545f057af23963f33c
Robert Neville <gcab012204@coderacademy.edu.au>
2022-04-17 20:50:06 +1000

    Add fetch_coming_week and translate_wind_direction methods

--- src/lib/weather.rb
+++ src/lib/weather.rb
@@ -2,6 +2,7 @@ require "http"
 
 module Weather
   API_URL = "https://api.open-meteo.com/v1/forecast?"
+  DEGREES_IN_COMPASS = 360.0
   WIND_DIRECTIONS = [
     "north", 
     "north-northeast", 
@@ -12,8 +13,29 @@ module Weather
     "southeast",
     "south-southeast",
     "south",
-
+    "south-southwest",
+    "southwest",
+    "west-southwest",
+    "west",
+    "west-northwest",
+    "northwest",
+    "north-northwest"
   ]
+  DAILY_WEATHER_VARIABLES = [
+    "weathercode",
+    "temperature_2m_max",
+    "temperature_2m_min",
+    "apparent_temperature_max",
+    "apparent_temperature_min",
+    "sunrise",
+    "sunset",
+    "precipitation_sum",
+    "precipitation_hours",
+    "windspeed_10m_max",
+    "winddirection_10m_dominant",
+    "shortwave_radiation_sum"
+  ]
+  DEGREES_IN_COMPASS_SEGMENT = DEGREES_IN_COMPASS / WIND_DIRECTIONS.length
 
   def Weather.fetch_current(timezone, latitude, longitude)
     latitude_param = "latitude=#{latitude}"
@@ -36,13 +58,41 @@ module Weather
         "time" => current_weather["time"], 
         "temp" => current_weather["temperature"], 
         "wind_speed" => current_weather["windspeed"],
-        "wind_direction" => current_weather["winddirection"],
+        "wind_direction" => self.translate_wind_direction(current_weather["winddirection"]),
         "weather" => self.translate_weather_code(current_weather["weathercode"])
       }
     end
   end
 
+  def Weather.fetch_coming_week(timezone, latitude, longitude)
+    latitude_param = "latitude=#{latitude}"
+    longitude_param = "longitude=#{longitude}"
+    daily_weather_variables = Weather.daily_weather_variables
+    timezone_param = "timezone=#{timezone}"
+    response = nil
+    begin
+      response = HTTP.get("#{API_URL}#{latitude_param}&#{longitude_param}&daily=#{daily_weather_variables}&#{timezone_param}")
+    rescue HTTP::ConnectionError
+      message = "Oops - there was a connection error. Make sure you're connected to the internet."
+      {"error" => message}
+    else
+      if response.status.server_error?
+        {"error" => "Oops - there was a server error. You might have to try again later."}
+      end
+      data = response.parse
+      {
+        "daily_units" => data["daily_units"],
+        "daily_variables" => data["daily"]
+      }
+    end
+  end
+
   private
+
+  def Weather.translate_wind_direction(wind_direction_degrees)
+    compass_segment = (wind_direction_degrees / DEGREES_IN_COMPASS_SEGMENT).floor
+    WIND_DIRECTIONS[compass_segment]
+  end
 
   def Weather.translate_weather_code(weather_code)
     case weather_code
@@ -103,6 +153,10 @@ module Weather
     when 99
       "thunderstorms with heavy hail" 
     end
+  end
+
+  def Weather.daily_weather_variables
+    DAILY_WEATHER_VARIABLES.join(",")
   end
 end
 
