53e361bb5fda680da7c66ab6b672ddfefc59b71d
Robert Neville <gcab012204@coderacademy.edu.au>
2022-04-22 06:00:39 +1000

    Add comments

--- src/lib/timezone.rb
+++ src/lib/timezone.rb
@@ -11,9 +11,13 @@ module Timezone
       message = "Oops - there was a connection error. Make sure you're connected to the internet."
       {"error" => message}
     else
+      # The fetch operation completed:
       if response.status.server_error?
+        # The request was received but there
+        # was a server error:
         {"error" => "Oops - there was a server error. You might have to try again later."}
       end
+      # Parse and return the data:
       data = response.parse
       {"timezone" => data["timezone"]}
     end
