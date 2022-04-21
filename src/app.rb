9ab04e2053c3a6c111c1772ba983b18e134fbd03
Robert Neville <gcab012204@coderacademy.edu.au>
2022-04-22 05:12:56 +1000

    Add more comments. Refactor

--- src/app.rb
+++ src/app.rb
@@ -187,21 +187,31 @@ class App
 
   def main_loop
     self.print_welcome_message
+    # Get timezone from API:
     timezone = self.get_timezone
     if !timezone
+      # No timezone - a connection error
+      # is likely to have occurred:
       Console.info("CLIMate needs to know your timezone to fetch forecasts.")
       self.exit_gracefully
     end
     begin
       while true
+        # Prompt user to select a location type:
         location_type = self.select_location_type
         location_info = nil
 
         case location_type
         when LOCAL
+          # Location type selected by the user is
+          # "Local" - see if they want to select
+          # a location from their saved local
+          # locations:
           location_info = self.select_location_from_user_locations
           if !location_info
-            # The user elected to search for a new location:
+            # Either the user has no saved local locations
+            # or they selected to search for a new one, so
+            # prompt them to enter a new one:
             message = "Enter a place name for your current location:"
             location_info = self.get_location_from_user(message)
             if location_info && !self.in_user_locations(location_info)
@@ -212,9 +222,14 @@ class App
             end
           end        
         when ELSEWHERE
+          # Location type selected by the user is
+          # "Elsewhere" - see if they want to select
+          # a location from their favourites:
           location_info = self.select_location_from_favourites
           if !location_info
-            # The user elected to search for a new location:
+            # Either the user has no favourites or they
+            # selected to search for a new location, so
+            # prompt them to enter a new one:
             message = "Enter a place name to search for:"
             location_info = self.get_location_from_user(message)
             if location_info && !self.in_favourites(location_info)
@@ -227,20 +242,31 @@ class App
         end
         
         if !location_info
+          # The user has searched for an invalid
+          # location name, and responded with
+          # no to "Try again?". Ask them if
+          # They would like to try a different
+          # location (continue using the app):
           if !Console.yes?("Would you like to view the weather for a different location?")
             self.exit_gracefully
           end
           next
         end
 
+        # Prompt the user to select a forecast type:
         forecast_type = self.select_forecast_type
         current_weather = nil
         weekly_forecast = nil
 
         case forecast_type
         when CURRENT
+          # Forecast type selected by the user is
+          # "Current" - get current weather from
+          # API:
           current_weather = self.get_current_weather(timezone, location_info)
           if !current_weather
+            # Could not fetch weather data. There was likely
+            # a connection error:
             message = "Sorry - CLIMate couldn't get weather data for #{location_info["display_name"]}"
             Console.info(message)
           else
@@ -248,23 +274,36 @@ class App
             self.print_current_weather(location_info["display_name"], current_weather)
           end
         when COMING_WEEK
+          # Forecast type selected by the user is
+          # "Coming Week" - get coming week weather
+          # from API:
           weekly_forecast = self.get_coming_week_weather(timezone, location_info)
           if !weekly_forecast
+            # Could not fetch weather data. There was likely
+            # a connection error:
             message = "Sorry - CLIMate couldn't get weather data for #{location_info["display_name"]}"
             Console.info(message)
           else
             Console.success("Success!")
+            # Prompt user to select an output
+            # type for the forecast:
             output_type = self.select_output_type
             case output_type
             when PRINT_TO_CONSOLE
+              # The user selected "Print to console":
               self.print_coming_week_weather(location_info["display_name"], weekly_forecast)
             when EXPORT_TO_PDF
+              # The user selected "Export to PDF":
               self.generate_forecast_pdf(location_info["display_name"], weekly_forecast)
             end
           end
         end
 
         if current_weather || weekly_forecast
+          # Either current or coming week weather data 
+          # has been successfully retrieved. Save this
+          # data to the user's history if they so
+          # desire:
           if Console.yes?("Would you like to save this weather data to your history?")
             if current_weather
               self.save_to_history(location_info["display_name"], forecast_type, current_weather)
@@ -279,6 +318,7 @@ class App
         end
       end
     rescue SignalException
+      # Catch CTRL+C
       self.exit_gracefully
     end
   end
@@ -293,6 +333,9 @@ class App
     Console.info("Determining your timezone...")
     response = Timezone.get()
     if response["error"]
+      # There was likely an connection error.
+      # Implicitly return nil when method
+      # terminates:
       Console.error(response["error"])
     else
       Console.success("Your timezone was detected as #{response["timezone"]}.")
@@ -301,6 +344,9 @@ class App
   end
 
   def select_location_type
+    # Prompt the user to select a
+    # location type. Return the
+    # selected option (string):
     message = "Please select a location type:"
     location_type_options = [LOCAL, ELSEWHERE]
     choice_index = Console.select(message, location_type_options)
@@ -308,13 +354,21 @@ class App
   end
 
   def select_location_from_user_locations
+    # There are no saved user locations,
+    # so return nil:
     return if self.user_locations.length == 0
+    # Create a list of selection options
+    # from the user locations' display names:
     options = []
     self.user_locations.each { |location| options.push(location["display_name"])}
-    options.push("Somewhere else")
+    options.push("Somewhere else") # At last index in array.
     message = "Please select one of the following saved locations or 'Somewhere else' to enter a new one:"
     choice_index = Console.select(message, options)
-    return if choice_index == options.length - 1
+    if choice_index == options.length - 1
+      # The user selected "Somewhere else",
+      # so return nil:
+      return
+    end
     self.user_locations[choice_index]
   end
 
@@ -327,6 +381,9 @@ class App
   end
 
   def save_user_location(location_info)
+    # Wrapper function for @config_manager.add_user_location,
+    # which displays an appropriate message based on the
+    # outcome:
     error = @config_manager.add_user_location(location_info)
     if error
       Console.error(error)
@@ -336,11 +393,15 @@ class App
   end
 
   def get_location_from_user(message)
+    # Prompt the user to enter a place name for
+    # a weather forecast:
     places_found = self.place_name_search_loop(message)
     return if !places_found
+    # Create a list of selection options
+    # from the found places' display names:
     options = []
     places_found.each { |place| options.push(place["display_name"]) }
-    options.push("None of these")
+    options.push("None of these") # At last index in array.
     message = "Please choose the correct location from the following list of alternatives:"
     choice_index = Console.select(message, options)
     if choice_index == options.length - 1
@@ -353,15 +414,23 @@ class App
 
   def place_name_search_loop(message)
     while true
+      # Get place name from user:
       place_name = Console.ask(message)
       Console.info("Searching for '#{place_name}' geocode info...")
       search_results = Geocode.search_places_by_name(place_name)
       if search_results["error"]
+        # There was an error. Display the
+        # error and exit the loop - 
+        # implicitly return nil when loop
+        # terminates:
         Console.error(search_results["error"])
         break if !Console.yes?("Try again?")
       else
+        # Destructure places found from results:
         places_found = search_results["places_found"]
         if places_found.length == 0
+          # No places found, so implicitly
+          # return nil when loop terminates:
           Console.info("Sorry - CLIMate couldn't find location info for '#{place_name}'.")
           break if !Console.yes?("Try again?")
         else
@@ -372,13 +441,18 @@ class App
   end
 
   def select_location_from_favourites
+    # No favourites - return nil:
     return if self.favourites.length == 0
+    # Ask if the user would like to select
+    # from their list of favourites:
     message = "Would you like to choose a location from your favourites?"
     select_from_favourites = Console.ask(message)
     return if !select_from_favourites
+    # Prompt the user to select from their
+    # list of favourites:
     options = []
     self.favourites.each { |place| options.push(place["display_name"]) }
-    options.push("Search for new location")
+    options.push("Search for new location") # At last index in array.
     message = "Here are your favourite locations. Please make a selection:"
     choice_index = Console.select(message, options)
     if choice_index == options.length - 1
@@ -398,6 +472,9 @@ class App
   end
 
   def save_favourite(location_info)
+    # Wrapper function for @config_manager.add_favourite,
+    # which displays an appropriate message based on the
+    # outcome:
     error = @config_manager.add_favourite(location_info)
     if error
       Console.error(error)
@@ -407,6 +484,9 @@ class App
   end
 
   def select_forecast_type
+    # Prompt the user to select a
+    # forecast type. Return the
+    # selected option (string):
     message = "Please select a weather forecast type:"
     forecast_type_options = [CURRENT, COMING_WEEK]
     choice_index = Console.select(message, forecast_type_options)
@@ -414,6 +494,9 @@ class App
   end
 
   def get_current_weather(timezone, location_info)
+    # Get current weather data from the API.
+    # Return nil if no data is returned, otherwise
+    # return the data:
     Console.info("Fetching current weather data for #{location_info["display_name"]}...")
     results = Weather.fetch_current(
       timezone, 
@@ -430,6 +513,8 @@ class App
   def print_current_weather(place_name, current_weather)
     Console.info("Current weather for #{place_name}:")
     date_time = current_weather["time"].split("T")
+    # Print each of the weather variables in the
+    # format specified in Console.print_weather_field:
     Console.print_weather_field("Date", date_time[0])
     Console.print_weather_field("Hour", date_time[1])
     Console.print_weather_field("Prevailing Conditions", current_weather["weather"])
@@ -440,6 +525,9 @@ class App
   end
 
   def get_coming_week_weather(timezone, location_info)
+    # Get coming week weather data from the API.
+    # Return nil if no data is returned, otherwise
+    # return the data:
     Console.info("Fetching the coming week's weather data for #{location_info["display_name"]}...")
     results = Weather.fetch_coming_week(
       timezone, 
@@ -454,6 +542,9 @@ class App
   end
 
   def select_output_type
+    # Prompt the user to select an ouput
+    # type for weather data. Return the
+    # selected option (string):
     message = "How would you like to view the results?"
     output_type_options = [PRINT_TO_CONSOLE, EXPORT_TO_PDF]
     choice_index = Console.select(message, output_type_options)
@@ -463,6 +554,8 @@ class App
   def print_coming_week_weather(place_name, weather_data)
     Console.info("Coming week's weather for #{place_name}:")
     weather_data.each do |daily_forecast|
+      # Print the weather variables for each day
+      # in the format specified in Console.print_weather_field:
       Console.print_weather_field(
         "Date", 
         daily_forecast["time"]["value"]
@@ -520,6 +613,8 @@ class App
   end
 
   def weather_variable_to_s(weather_variable)
+    # Return a weather variable's value followed
+    # by its corresponding unit separated by a space:
     "#{weather_variable["value"]} #{weather_variable["unit"]}"
   end
 
@@ -528,11 +623,14 @@ class App
     while !filename
       filename_input = Console.ask("Please enter a name for the file:")
       if filename_input == "." || filename_input == ".."
+        # Filename input is a special directory on Unix based systems.
         Console.error("'.' and '..' are reserved filenames.")
         next
       end
       add_pdf_extension = true
       if filename_input.match(/\.pdf$/)
+        # No PDF extension was included
+        # in the user's input.
         add_pdf_extension = false
       end
       basename = filename_input + (add_pdf_extension ? ".pdf" : ""
@@ -541,10 +639,13 @@ class App
     
     proceed = true
     if File.exist?(filename)
+      # The file already exists. Ask the user
+      # if they want to overwrite it:
       proceed = Console.yes?("'#{filename}' already exists. Do you want to overwrite it?")
     end
 
     if proceed
+      # Generate weekly forecast PDF:
       Prawn::Fonts::AFM.hide_m17n_warning = true
       Prawn::Document.generate(filename) do |pdf|
         pdf.text "Weekly Weather Forecast for #{place_name}"
@@ -553,6 +654,8 @@ class App
         pdf.move_down 20
         pdf.font "Courier"
         weather_data.each do |daily_forecast|
+          # Generate formatted fields for each weather
+          # variable and add to PDF text:
           pdf.text self.get_weather_field_string(
             "Date", 
             daily_forecast["time"]["value"]
@@ -610,11 +713,15 @@ class App
       end
       Console.success("Successfully exported weekly forecast to #{filename}!")
     else
+      # The user does not want to overwrite
+      # the file that already exists with
+      # the same name as their input:
       Console.info("Aborting PDF generation...")
     end
   end
 
   def get_weather_field_string(label, value)
+    # Returns a string formatted as "label-------------------------> value"
     weather_field_string = label
     weather_field_string += ("-" * (30 - label.length) + "> ")
     weather_field_string += value
