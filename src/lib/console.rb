56140712884b4cf55e8bc12509133151eaf97243
Robert Neville <gcab012204@coderacademy.edu.au>
2022-04-22 05:51:33 +1000

    Add comments

--- src/lib/console.rb
+++ src/lib/console.rb
@@ -3,21 +3,26 @@ require "colorize"
 
 module Console
   def Console.info(message)
+    # Print message in white:
     puts message
     puts
   end
 
   def Console.error(message)
+    # Print message in red:
     puts message.red
     puts
   end
 
   def Console.success(message)
+    # Print message in green:
     puts message.green
     puts
   end
 
   def Console.ask(message)
+    # Ask the user a question (message)
+    # and return their answer:
     prompt = TTY::Prompt.new
     answer = prompt.ask(message)
     puts
@@ -25,6 +30,8 @@ module Console
   end
 
   def Console.yes?(message)
+    # Ask the user a yes/no question (message)
+    # and return the boolean response:
     prompt = TTY::Prompt.new
     answer = prompt.yes?(message)
     puts
@@ -32,6 +39,10 @@ module Console
   end
 
   def Console.select(message, options)
+    # Prompt the user to select from
+    # options with a given message.
+    # This method returns the index of
+    # the user's choice in options:
     prompt = TTY::Prompt.new
     choice = prompt.select(message, options)
     options.each_with_index do |option, index|
@@ -43,6 +54,8 @@ module Console
   end
   
   def Console.print_weather_field(label, value)
+    # Print a weather variable in the
+    # fomat of "variable_name------> value":
     print label.yellow
     print ("-" * (30 - label.length) + "> ").blue
     print value
